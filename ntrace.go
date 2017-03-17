bfc8ec165d354864a48320ace0521d179d17f461
Tom <oktopac@users.noreply.github.com>
2017-03-17 14:07:13 +0000

    get rid of the filter

--- ntrace.go
+++ ntrace.go
@@ -15,12 +15,10 @@ func main() {
 
 	if handle, err := pcap.OpenLive("en0", 1600, true, pcap.BlockForever); err != nil {
 		panic(err)
-	} else if err := handle.SetBPFFilter("tcp and port 80"); err != nil { // optional
-		panic(err)
 	} else {
 		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 		for packet := range packetSource.Packets() {
-			handlePacket(packet) // Do something with a packet here.
+			handlePacket(packet)
 		}
 	}
 }
